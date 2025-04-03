9e6e540516dfb483e79a829423e6f2dcd14444bc
GGTuran <tayyabturan67@gmail.com>
2025-04-03 15:58:35 +0600

    streamlit

---
+++ Streamlit/app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Hello World!!")
+
+st.write("Simple line should be added here")
+
+# simple data frame 
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+
+st.write("Here is the dataframe")
+st.write(df)
+
+
+# Creating a line chart 
+chart_data = pd.DataFrame(
+    np.random.randn(20,3),columns=['a','b','c']
+)
+st.line_chart(chart_data)
